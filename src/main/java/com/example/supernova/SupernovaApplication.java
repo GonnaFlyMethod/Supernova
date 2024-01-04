0ffa32ffb03e8636860026a9b647f2a48e763477
GonnaFlyMethod <alex.husakou@gmail.com>
2024-01-04 22:28:09 +0100

    files/add-java-files-for-project

---
+++ src/main/java/com/example/supernova/SupernovaApplication.java
@@ -0,0 +1,13 @@
+package com.example.supernova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupernovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupernovaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/supernova/SupernovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.supernova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupernovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
